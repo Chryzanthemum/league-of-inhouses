43ae7cc6ff5cec271bef85dd5a152e948e1a57fe
Benjamin Jiang <sydelleailey93589@gmail.com>
2020-09-04 15:01:44 -0400

    Create generate_teams.py

---
+++ generate_teams.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import random
+import fire
+
+
+class Teams:
+  # Look up player IDs from spreadsheet
+  # >>> python teams.py random_teams [0, 1, 2, 3, 4, 6, 12, 9, 10, 11]
+  def random_teams(players):
+    random.shuffle(players)
+
+    blue = players[:5]
+    red = players[5:]
+    
+    print('blue:', blue)
+    print('red:', red)
+
+if __name__ == "__main__":
+    fire.Fire(Teams)
