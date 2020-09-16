fbd4e03136c053d4ac84e18d023fa0ac20fc0044
Benjamin JIang <benjamin.jiang@well.co>
2020-09-16 11:47:45 -0400

    This doesnt work lol

--- generate_teams.py
+++ generate_teams.py
@@ -1,20 +1,19 @@
-import pandas as pd
-import numpy as np
 import random
 import fire
 
 
 class Teams:
-  # Look up player IDs from spreadsheet
-  # >>> python teams.py random_teams [0, 1, 2, 3, 4, 6, 12, 9, 10, 11]
-  def random_teams(players):
-    random.shuffle(players)
+    # Look up player IDs from spreadsheet
+    # >>> python teams.py random_teams [0, 1, 2, 3, 4, 6, 12, 9, 10, 11]
+    def random_teams(self, players):
+        random.shuffle(players)
 
-    blue = players[:5]
-    red = players[5:]
-    
-    print('blue:', blue)
-    print('red:', red)
+        blue = players[:5]
+        red = players[5:]
+
+        print('blue:', blue)
+        print('red:', red)
+
 
 if __name__ == "__main__":
     fire.Fire(Teams)
